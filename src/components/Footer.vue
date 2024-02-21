a48228c8787255db69e0622f0e9223f98f56bba9
Kazuma Saito <kazu15870@gmail.com>
2024-02-21 12:32:38 -0800

    feat: Made top view

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center text-sm">
+    Designer <span>Masaki Romero</span>, Supporter <span>Kazuma Saito</span>,
+    Sponsor
+    <spna>Tanayu</spna>
+  </div>
+</template>
