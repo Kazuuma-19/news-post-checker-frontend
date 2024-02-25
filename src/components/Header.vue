a75cddee31049595e93bfea751d9104949b93f34
Kazuma Saito <kazu15870@gmail.com>
2024-02-25 08:45:34 -0800

    feat: added router-link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,8 +10,10 @@ const toggleMenu = () => {
 <template>
   <header class="mb-12 relative">
     <h1 class="text-center text-2xl sm:text-3xl lg:text-4xl">
-      News Posts
-      <span class="text-white bg-main-color-blue py-2 px-3">Checker</span>
+      <router-link to="/">
+        News Posts
+        <span class="text-white bg-main-color-blue py-2 px-3">Checker</span>
+      </router-link>
     </h1>
 
     <nav
@@ -22,7 +24,7 @@ const toggleMenu = () => {
         class="absolute lg:static top-1/2 left-1/2 -translate-x-1/2 lg:translate-x-0 -translate-y-1/2 lg:translate-y-0 flex flex-col lg:flex-row items-center gap-10 lg:gap-8 text-4xl lg:text-base"
       >
         <li class="border-b lg:border-none border-gray-700">
-          <a href="#" class="flex items-center gap-1">
+          <router-link to="/" class="flex items-center gap-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -38,10 +40,10 @@ const toggleMenu = () => {
               />
             </svg>
             Home
-          </a>
+          </router-link>
         </li>
         <li class="border-b lg:border-none border-gray-700">
-          <a href="/students" class="flex items-center gap-1">
+          <router-link to="/students" class="flex items-center gap-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -56,8 +58,8 @@ const toggleMenu = () => {
                 d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
               />
             </svg>
-            Student</a
-          >
+            Student
+          </router-link>
         </li>
       </ul>
     </nav>
