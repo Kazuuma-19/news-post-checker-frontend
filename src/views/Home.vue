3e72124d9d0fd91f5c7a04091726d81594a7d050
Kazuma Saito <kazu15870@gmail.com>
2024-02-27 09:59:55 -0800

    feat: created student view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,7 +64,7 @@
     </div>
   </div>
 
-  <div class="my-16">
+  <div class="my-12">
     <div class="relative w-full min-w-[200px] h-10 mb-4">
       <input
         class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
