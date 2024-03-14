d45d70787afb023708e8590dce03b92b45afcb29
Kazuma Saito <kazu15870@gmail.com>
2024-03-14 13:22:06 -0700

    style: Changed the style of the checkbox

--- src/components/CreateModal.vue
+++ src/components/CreateModal.vue
@@ -67,7 +67,7 @@ const addStudent = async () => {
         v-model="student.grade"
         :options="grades"
         label="学年"
-        class="mb-4"
+        class="mb-6"
       />
       <div class="flex items-center">
         <input
@@ -75,7 +75,7 @@ const addStudent = async () => {
           v-model="student.active"
           id="edit-checked-checkbox"
           type="checkbox"
-          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+          class="w-4 h-4 text-main-color-blue bg-gray-100 border-gray-300 rounded focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
         />
         <label
           for="edit-checked-checkbox"
