40373b3b26ad51d94d541b0a5f0476409a34d429
Kazuma Saito <kazu15870@gmail.com>
2024-03-17 14:37:39 -0700

    feat: Changed router and posts to a ts file

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,27 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "/src/views/Home.vue";
-import Student from "/src/views/Student.vue";
-import Result from "/src/views/Result.vue";
+import Home from "../views/Home.vue";
+import Student from "../views/Student.vue";
+import Result from "../views/Result.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/students",
+    name: "Student",
+    component: Student,
+  },
+  {
+    path: "/results",
+    name: "Result",
+    component: Result,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: Home,
-    },
-    {
-      path: "/students",
-      name: "Student",
-      component: Student,
-    },
-    {
-      path: "/results",
-      name: "Result",
-      component: Result,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,11 +1,28 @@
 import { defineStore } from "pinia";
 
+interface Post {
+  id: number;
+  name: string;
+  grade: "FIRST_YEAR" | "SECOND_YEAR" | "THIRD_YEAR" | "FOURTH_YEAR";
+  active: boolean;
+  created_at: string;
+  updated_at: string;
+  reply: {
+    count: number;
+    dateTime: string[];
+  };
+  post: {
+    count: number;
+    dateTime: string[];
+  };
+}
+
 export const usePostsStore = defineStore("posts", {
   state: () => ({
-    response: null,
+    response: [] as Post[],
   }),
   actions: {
-    setResponse(response) {
+    setResponse(response: Post[]) {
       this.response = response;
     },
   },
